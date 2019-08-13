b31228550eb0fbfdf831b21c2c769d30e24a905b
Jordan Fowler <me@jordanfowler.co>
2019-08-13 17:55:46 -0600

    Added Orderbook to support Robinhood Gold's Nasdaq Level II market data.

---
+++ orderbook.go
@@ -0,0 +1,34 @@
+package robinhood
+
+// Orderbook is a representation of the data returned by the Robinhood Gold API for
+// Nasdaq Level II bid/ask order book data
+type Orderbook struct {
+	Symbol string  `json:"symbol"`
+	Asks   []Order `json:"asks"`
+	Bids   []Order `json:"bids"`
+}
+
+// Order represents a single order in an Orderbook
+type Order struct {
+	Side     string `json:"side"`
+	Price    Price  `json:"price"`
+	Quantity uint64 `json:"quantity"`
+}
+
+// Price is the Order price
+type Price struct {
+	Amount       string `json:"amount"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+// GetOrderbook returns the latest order book for the given ticker symbol
+func (c *Client) GetOrderbook(sym string) (*Orderbook, error) {
+	instr, err := c.GetInstrumentForSymbol(sym)
+	if err != nil {
+		return nil, err
+	}
+	url := EPMarketbook + instr.ID
+	var r Orderbook
+	err = c.GetAndDecode(url, &r)
+	return &r, err
+}
---
+++ orderbook_test.go
@@ -0,0 +1,33 @@
+package robinhood
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOrderbook(t *testing.T) {
+	if os.Getenv("ROBINHOOD_USERNAME") == "" {
+		t.Skip("No username set")
+		return
+	}
+	asrt := assert.New(t)
+	o := &OAuth{
+		Username: os.Getenv("ROBINHOOD_USERNAME"),
+		Password: os.Getenv("ROBINHOOD_PASSWORD"),
+	}
+
+	c, err := Dial(&CredsCacher{Creds: o})
+
+	asrt.NoError(err)
+	asrt.NotNil(c)
+
+	ob, err := c.GetOrderbook("SPY")
+	asrt.NoError(err)
+
+	spew.Dump(ob)
+	fmt.Printf("ob.Asks = %+v ob.Bids = %+v\n", len(ob.Asks), len(ob.Bids))
+}
